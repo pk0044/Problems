e1922f7319d4854a8a69d975d1943ba9989ec883
pk0044 <purushottam0044@gmail.com>
2020-04-20 20:41:57 +0530

    adding leetcode problem

---
+++ src/com/pk/Problems/ConstructBSTFromPreOrder.java
@@ -0,0 +1,29 @@
+package com.pk.Problems;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class ConstructBSTFromPreOrder {
+	
+	private class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x){
+			val = x;
+		}
+	}
+	
+    public TreeNode bstFromPreorder(int[] preorder) {
+    	
+        return null;
+    }
+}
